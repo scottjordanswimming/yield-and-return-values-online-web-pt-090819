656271e27bad204e5d930a7be41055689cc90623
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-21 19:40:56 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -4,10 +4,9 @@ def hello(array)
   i = 0
   collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
+  collection
 end
 
-binding.pry
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
